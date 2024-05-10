62e9b5cce985160484e8d83888f9bda88d7e0590
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-05-10 09:20:31 -0600

    se agregaron las rutas de los endPoints

--- src/publication.routes.js
+++ src/publication.routes.js
@@ -5,3 +5,24 @@ import {
     publicationGet,
     publicationPost,
 } from './publication.controller.js';
+
+import { validateFields } from '../middlewares/validate-fields.js';
+
+const router = Router();
+
+router.get('/', publicationGet);
+
+router.post(
+    "/add",
+    [
+        check("author", "Author is required").not().isEmpty(),
+        check("title", "Title is required").not().isEmpty(),
+        check("description", "Description is required").not().isEmpty(),
+        check("tools", "Tools is required").not().isEmpty(),
+        check("descriptionFuntion", "DescriptionFuntion is required").not().isEmpty(),
+        check("image", "Image is required").not().isEmpty(),
+        check("link", "Link is required").not().isEmpty(),
+        validateFields
+    ], publicationPost);
+
+export default router;
