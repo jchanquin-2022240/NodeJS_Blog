eae5a4f13d0ca8f272143e3df7ce26812019e273
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-05-10 00:19:53 -0600

    se crearon los archivos de publication y se trabajo model

---
+++ src/publication.model.js
@@ -0,0 +1,39 @@
+
+import mongoose from "mongoose";
+
+const PublicationSchema = mongoose.Schema({
+    author: {
+        type: String,
+        requered: [true, "Post author is required"]
+    },
+    title: {
+        type: String,
+        required: [true, "Post title is required"]
+    },
+    description: {
+        type: String,
+        required: [true, "Post description is required"]
+    },
+    tools: {
+        type: String,
+        required: [true, "Post tools is required"]
+    },
+    descriptionFuntion: {
+        type: String,
+        required: [true, "Post descriptionFuntion is required"]
+    },
+    image: {
+        type: URL,
+        required: [true, "Post image is required"]
+    },
+    link: {
+        type: URL,
+        required: [true, "Post URL is required"]
+    },
+    publicationStatus: {
+        type: Boolean,
+        default: true
+    }
+});
+
+export default mongoose.model('Publication', PublicationSchema);
