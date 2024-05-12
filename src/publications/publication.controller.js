ac85087db4f9ee526012db27048d72a31a354784
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-05-12 12:28:46 -0600

    comentado endPoint

--- src/publications/publication.controller.js
+++ src/publications/publication.controller.js
@@ -34,32 +34,39 @@ export const publicationGet = async (req, res) => {
     res.status(200).json({ msg: "Publications found", totalPublication, publications });
 }
 
-export const publicationPost = async (req, res) => {
-    const { author, title, description, tools, descriptionFuntion, image, link, date } = req.body;
-
-    const publication = new Publication({
-        author,
-        title,
-        description,
-        tools,
-        descriptionFuntion,
-        image,
-        link,
-        date
-    });
-
-    await publication.save();
-
-    res.status(200).json({ msg: "Publication created", publication });
+export const publicationGetComment = async ( res) => {
+    try {
+        const publications = await Publication.find();
+        res.status(200).json(publications);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
+
+// export const publicationPost = async (req, res) => {
+//     const { author, title, description, tools, descriptionFuntion, image, link, date } = req.body;
+
+//     const publication = new Publication({
+//         author,
+//         title,
+//         description,
+//         tools,
+//         descriptionFuntion,
+//         image,
+//         link,
+//         date
+//     });
+
+//     await publication.save();
+
+//     res.status(200).json({ msg: "Publication created", publication });
+// }
 
 export const AddCommentPut = async (req, res) => {
     const { id} = req.params;
 
     const { _id, author, title, description, tools, descriptionFuntion, image, link, date, ...rest} = req.body;
-    const currentDate = new Date();
     const addComment = { ...rest};
-    addComment.dateComment = currentDate;
 
     await Publication.findByIdAndUpdate(id, { $push: { comments: addComment } });
     res.status(200).json({ msg: "Comment added" });
